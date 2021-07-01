97bf5b3090da58256cd463e3307bb7bc5206a858
Kenadi Yarovoy <yarovoykirill942@gmail.com>
2021-07-01 03:28:38 +0300

    old cinemamachine camera

--- Assets/Script/Game/LoadScene.cs
+++ Assets/Script/Game/LoadScene.cs
@@ -73,7 +73,7 @@ namespace Game
             if(array.Length != 0)
             {
                 if(array.Length > 1)
-                    Destroy(array[1]);
+                    Destroy(destroyOrNot);
 
                 DontDestroyOnLoad(destroyOrNot);
             }
